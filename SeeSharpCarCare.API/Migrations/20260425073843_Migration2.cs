9b428826d27081160c580125aa463650d2f0e44f
Dulaya <dulaya.saennok@gmail.com>
2026-04-26 15:48:21 -0700

    Added migration

---
+++ SeeSharpCarCare.API/Migrations/20260425073843_Migration2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeeSharpCarCare.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Vehicles_VIN",
+                table: "Invoices");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Vehicles_VIN",
+                table: "Invoices",
+                column: "VIN",
+                principalTable: "Vehicles",
+                principalColumn: "VIN",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Vehicles_VIN",
+                table: "Invoices");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Vehicles_VIN",
+                table: "Invoices",
+                column: "VIN",
+                principalTable: "Vehicles",
+                principalColumn: "VIN",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
